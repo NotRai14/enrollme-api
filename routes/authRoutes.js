ad084b26575128d4a42de01313aa84c340e3815e
NotRai14 <arpitpatel0014@gmail.com>
2026-02-24 18:20:05 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login } = require("../controllers/authController");
+const authController = require("../controllers/authController");
 
-router.post("/register", register);
-router.post("/login", login);
+// REGISTER
+router.post("/register", authController.register);
+
+// LOGIN
+router.post("/login", authController.login);
 
 module.exports = router;
